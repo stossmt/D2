27751cbd2617ed2e5fa6d0a2fcc7004e47a6a21b
Matt Stoss <mts74@pitt.edu>
2019-02-20 17:37:15 -0500

    Project setup

---
+++ tests/sample_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+
+class SampleTest < Minitest::Test
+
+  # Tests that our num_chars method works with short names like "Bob"
+  def test_sample
+    assert_equal 3, 3
+  end
+end
