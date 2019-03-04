a5c0c5e179a150a88c0263728d9ea776f2c9a3b7
Matt Stoss <mts74@pitt.edu>
2019-03-03 20:34:55 -0500

    final

--- tests/location_test.rb
+++ tests/location_test.rb
@@ -7,8 +7,15 @@ class LocationTest < Minitest::Test
   # Location is just a data class, so there is not any methods to test. 
   # Test a successful initialization for code coverage
 
-  def test_location_success
-    assert_instance_of Location, Location.new("name", 1, 2)
+  # The second parameter should properly set max_real
+  def test_location_max_real
+  	l = Location.new("name", 1, 2)
+    assert_equal 1, l.max_real
   end
 
+  # The third parameter should properly set max_fake
+  def test_location_max_fake
+  	l = Location.new("name", 1, 2)
+    assert_equal 2, l.max_fake
+  end
 end
